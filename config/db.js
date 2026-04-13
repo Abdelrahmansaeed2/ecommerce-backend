494c8fc1babe556f905645fc1102fca72a66286b
abdelrahmansaeed <abdelrahmansaeed26022020@gmail.com>
2026-04-13 11:30:09 +0200

    feat(middleware): initialize system setup
    - setup express application
    - configure mongodb connection using mongoose
    - add environment variables configuration
    - prepare server entry point

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URL);
     console.log("MongoDB connected");
+    
   } catch (error) {
     console.error(error);
     process.exit(1);
